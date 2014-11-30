90784836ccd7c92e9418e2d8cdef5e7bc6cc0faf
shausler <shausler1115@gmail.com>
2014-11-29 20:55:05 -0600

    Removed useless button

--- gen/com/whiterabbitplanner/R.java
+++ gen/com/whiterabbitplanner/R.java
@@ -34,15 +34,14 @@ public final class R {
         public static final int roundedbutton=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f090010;
-        public static final int buttonBar=0x7f09000e;
-        public static final int button_cancel=0x7f09000b;
+        public static final int action_settings=0x7f09000f;
+        public static final int buttonBar=0x7f09000d;
         public static final int button_insert=0x7f09000a;
         public static final int calendarView1=0x7f090001;
         public static final int email=0x7f090004;
         public static final int fragment_contact_inserter=0x7f090002;
-        public static final int fragment_contacts=0x7f09000c;
-        public static final int list=0x7f09000d;
+        public static final int fragment_contacts=0x7f09000b;
+        public static final int list=0x7f09000c;
         public static final int name=0x7f090003;
         public static final int notes=0x7f090008;
         public static final int pager=0x7f090000;
@@ -50,7 +49,7 @@ public final class R {
         public static final int postal=0x7f090009;
         public static final int secondEmail=0x7f090005;
         public static final int secondPhone=0x7f090007;
-        public static final int to_inserter=0x7f09000f;
+        public static final int to_inserter=0x7f09000e;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
